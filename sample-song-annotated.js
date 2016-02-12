278df9caec213e7ba0364c8328a49493e282c965
artjacob <aacj03@gmail.com>
2016-02-12 20:36:37 -0200

    Annotated sample

---
+++ sample-song-annotated.js
@@ -0,0 +1,91 @@
+[
+	"Tmn3fcmedhyngybtocuvmtyypce", // [0] Google Play song identifier (also used for preview url)
+	"Bye Bye Bye", // [1] Song title
+	"https://lh3.googleusercontent.com/rOu9s0D5qlgMtFBfrGKqMdUB7yVBJX4CIg3QYRuPFCJY2IpdFOdh09QEAeZEPc-QUG8mfLho", // [2] Cover image
+	"*NSYNC", // [3] Artist
+	"No Strings Attached", // [4] Album
+	"*NSYNC", // [5] Album artist
+	null, // [6] ?
+	null, // [7] ?
+	null, // [8] ?
+	null, // [9] ?
+	"", // [10] ?
+	"Pop", // [11] Genre
+	null, // [12] ?
+	200000, // [13] Length (in seconds)
+	1, // [14] Song's track number on album
+	null, // [15] ?
+	1, // [16] ?
+	null, // [17] ?
+	2000, // [18] Year
+	null, // [19] ?
+	null, // [20] ?
+	null, // [21] ?
+	9, // [22] Play count
+	5, // [23] Thumbs up (5-star rating)
+	0, // [24] ?
+	0, // [25] ?
+	null, // [26] ?
+	"Tmn3fcmedhyngybtocuvmtyypce", // [27] Google Play song identifier (repeated)
+	"Tmn3fcmedhyngybtocuvmtyypce", // [28] Google Play song identifier (repeated)
+	5, // [29] ?
+	null, // [30] ?
+	null, // [31] ?
+	"Bt45mxbgck76bhhgehccgqlo6ua", // [32] Google Play album identifier
+	"Agqztbhnf7i2usm6bajffngqf7a", // [33] Google Play artist identifier
+	null, // [34] ?
+	null, // [35] ?
+	"https://lh3.googleusercontent.com/bxgvmMLCn6Iroo9A3K9GXXfzdzmbI7YuhdVMzPvWoY3GWmjBznDkp57-QPNC70aDXQAlI582qQ", // [36] Artist photo
+	null, // [37] ?
+	2, // [38] ?
+	null, // [39] ?
+	"AE9vGKrnCiJKt-7U_TSCMc9HWzNYxQwK8tmGtj613ZTqg6S0xTrhTPdjY4_tgl5fX3I72LmHe47fIDFkY2tfS62jrjMyUO-5HVdqPMoOCrk5T7hoPYCh4wM=", // [40] ?
+	null, // [41] ?
+	null, // [42] ?
+	"32576e58-9bf6-3fa1-b9d4-350e7f8f8e31", // [43] ?
+	null, // [44] ?
+	[ // [45] ?
+		null, // [0]
+		null, // [1]
+		null, // [2]
+		"AE9vGKrnCiJKt-7U_TSCMc9HWzNYxQwK8tmGtj613ZTqg6S0xTrhTPdjY4_tgl5fX3I72LmHe47fIDFkY2tfS62jrjMyUO-5HVdqPMoOCrk5T7hoPYCh4wM=", // [3] ? (repeated)
+		null, // [4]
+		null, // [5]
+		null, // [6]
+		"https://play.google.com/store/music/album/?authuser=0&id=Bt45mxbgck76bhhgehccgqlo6ua" // [7] Albums's url on Google Play store (contains album identifier)
+	],
+	null, // [46] ?
+	null, // [47] ?
+	null, // [48] ?
+	1437707338606000, // [49] ?
+	"Tmn3fcmedhyngybtocuvmtyypce", // [50] Google Play song identifier (repeated)
+	[ // [51] ?
+		1, // [0] ?
+		1, // [1] ?
+		0 // [2] ?
+	],
+	"Eo-KmOd3i7s", // [52] YouTube identifier
+	null, // [53] ?
+	null, // [54] ?
+	[ // [55] YouTube
+		"Eo-KmOd3i7s", // [0] YouTube identifier (repeated)
+		"https://i.ytimg.com/vi/Eo-KmOd3i7s/mqdefault.jpg" // [1] YouTube thumbnail
+	],
+	0, // [56] ?
+	null, // [57] ?
+	[ // [58] Google Play store price
+		2290000, // [0] Song price * 1000000
+		"BRL" // [1] Currency
+	],
+	[ // [59] ?
+		[ // [0] ?
+			"http://lh3.googleusercontent.com/bxgvmMLCn6Iroo9A3K9GXXfzdzmbI7YuhdVMzPvWoY3GWmjBznDkp57-QPNC70aDXQAlI582qQ", // [0] Artist photo (repeated)
+			0, // [1]
+			2 // [2]
+		]
+	],
+	null, // [60] ?
+	null, // [61] ?
+	null, // [62] ?
+	1 // [63] ?
+]
