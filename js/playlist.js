93f26a52cacfba43fcf10e6a82e26b259e4b20a8
artjacob <aacj03@gmail.com>
2016-02-11 22:47:58 -0200

    Data folder renamed to avoid conflict with Jekyll

--- js/playlist.js
+++ js/playlist.js
@@ -1,7 +1,8 @@
 $(function() {
 	var $table = $("table tbody");
+	var $pre = $("pre");
 
-	$.getJSON("_data/400.json").done(function(data) {
+	$.getJSON("data/400.json").done(function(data) {
 		var songs = data[1][0];
 		var count = 1;
 
@@ -10,8 +11,14 @@ $(function() {
 			$("<td />").html(count).appendTo($song);
 			$("<td />").html(value[1]).appendTo($song);
 			$("<td />").html(value[3]).appendTo($song);
+			$("<td />").html((value[13] / 1000)).appendTo($song);
+			$("<td />").html(value[18]).appendTo($song);
 
 			$song.appendTo($table);
+
+			$("<div />").text("#EXTINF:" + (value[13] / 1000) + "," + value[1]).appendTo($pre);
+			$("<div />").text("/Users/arthur/Music/" + value[1] + ".m4a").appendTo($pre);
+
 			count++;
 		});
 	});
